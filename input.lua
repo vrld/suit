1b67db04872df7795dcc11a207c92bad6eb02b51
Angelo Geels <spansjh@gmail.com>
2016-10-23 13:34:31 +0200

    Add force bool to input to force focus on first draw

--- input.lua
+++ input.lua
@@ -49,6 +49,11 @@ return function(core, input, ...)
 	end
 
 	-- user interaction
+	if input.force ~= nil and input.force then
+		core.active = opt.id
+		input.force = false
+	end
+
 	opt.state = core:registerHitbox(opt.id, x,y,w,h)
 	opt.hasKeyboardFocus = core:grabKeyboardFocus(opt.id)
 
